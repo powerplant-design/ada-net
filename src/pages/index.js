35d88007e6a778e647a03b685c178422ffd6e37e
Lucas Frey <lucas.frey@mobi2go.com>
2021-12-01 21:34:58 +1300

    feat(video): implement video on the homepage for synopsium events

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useEffect } from "react"
 import { Link } from "gatsby"
 import styled from "styled-components"
 
@@ -12,15 +12,35 @@ import IndexArtbase from "../components/indexArtbase"
 import ImgSymposium from "../assets/ada-sym-green.gif"
 
 const Home = () => {
+  useEffect(() => {
+    const symposiumVideo = document.getElementById("video")
+    const observer = new IntersectionObserver(
+      function (entries) {
+        if (entries[0].isIntersecting === true) symposiumVideo.play()
+      },
+      { threshold: [0] }
+    )
+
+    observer.observe(document.querySelector("#video"))
+  }, [])
+
   return (
     <Layout isHomePage>
       <Seo title="Homepage" />
       <TempSymposium>
         <div className="symposium-container">
-          <img
-            src={ImgSymposium}
-            alt="inderterminate infrastructure text behind green background"
-          />
+          <video
+            style={{ maxHeight: "82vh" }}
+            id="video"
+            width="100%"
+            height="100%"
+          >
+            <source src="ada-splash-compressed.mp4" type="video/mp4" />
+            <img
+              src={ImgSymposium}
+              alt="inderterminate infrastructure text behind green background"
+            />
+          </video>
           <Link className="symposium-link" to="/tag/symposium2021/">
             <h2>Pōneke/Wellington Symposium 2021</h2>
           </Link>
